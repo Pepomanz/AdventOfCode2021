c74e70bde6e26ce3d5f86b0da8fd3d3b5e5aebdd
pepomanz <pepoman1@gmail.com>
2021-12-09 22:47:01 +0700

    [ADD] DAY 1 CODE

--- src/com/company/Advent.java
+++ src/com/company/Advent.java
@@ -1,2 +1,56 @@
-package com.company;public class Advent {
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Advent {
+    public long day1(String input){
+        String[] inputList = input.split("\n");
+        List<Boolean> compareList = new ArrayList<>();
+        int i = 1;
+        while (i < inputList.length){
+            if(i > 0){
+                int prevValue = Integer.parseInt(inputList[i -1]);
+                int nextValue = Integer.parseInt(inputList[i]);
+                int result = nextValue - prevValue;
+                if(result > 0){
+                    compareList.add(true);
+                } else {
+                    compareList.add(false);
+                }
+            }
+            i++;
+        }
+        long countIncreaseValue = compareList.stream().filter(x -> x == true).count();
+        return countIncreaseValue;
+    }
+
+    public long day1WithExtra(String input){
+        List<Integer> inputList = Arrays.stream(input.split("\n")).map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+        List<Integer> dataList = new ArrayList<>();
+        List<Boolean> compareList = new ArrayList<>();
+
+        for(int i=0; i< inputList.size() -2 ; i++){
+            int total = inputList.get(i) +  inputList.get(i+1) +  inputList.get(i+2);
+            dataList.add(total);
+        }
+
+        int i = 1;
+        while (i < dataList.size()){
+            if(i > 0){
+                int prevValue = dataList.get(i -1);
+                int nextValue = dataList.get(i);
+                int result = nextValue - prevValue;
+                if(result > 0){
+                    compareList.add(true);
+                } else {
+                    compareList.add(false);
+                }
+            }
+            i++;
+        }
+        return compareList.stream().filter(x -> x == true).count();
+    }
 }
--- src/test/AdventTest.java
+++ src/test/AdventTest.java
@@ -1,2 +1,23 @@
-package test;public class AdventTest {
+package test;
+
+import com.company.Advent;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AdventTest {
+    private Advent advent = new Advent();
+
+    @Test
+    public void testAdvent1() {
+        String input = "199\n" + "200\n" + "208\n" + "210\n" + "200\n" + "207\n" + "240\n" + "269\n" + "260\n" + "263";
+        long result = advent.day1(input);
+        Assert.assertEquals(7, result);
+    }
+
+    @Test
+    public void testAdvent1WithExtra() {
+        String input = "199\n" + "200\n" + "208\n" + "210\n" + "200\n" + "207\n" + "240\n" + "269\n" + "260\n" + "263";
+        long result = advent.day1WithExtra(input);
+        Assert.assertEquals(5, result);
+    }
 }
